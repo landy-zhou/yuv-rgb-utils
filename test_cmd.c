e8c386cc42ddf3f1b2fbdf670a83bed6293cebc6
Qiang Zhou <qiangzhou@asrmicro.com>
2016-04-27 20:36:51 +0800

    add test yuv data & test code without opencv ui

---
+++ test_cmd.c
@@ -0,0 +1,81 @@
+
+#include <stdio.h>
+#include "rgb_yuv_utils.h"
+
+#define WIDTH 	1920
+#define HEIGHT	1080
+
+char yuv_buf[WIDTH*HEIGHT*2];
+char rgb_buf[WIDTH*HEIGHT*4];
+char out_yuv_buf[WIDTH*HEIGHT*3];
+
+int main(int argc,char **argv)
+{
+    FILE *yuv_file;
+    FILE *rgb_file;
+    FILE *out_yuv_file;
+    int width = WIDTH;
+    int height = HEIGHT;
+    struct timeval start_time,end_time;
+    char out_yuv_path[100];
+
+    if( 3 > argc )
+    {
+	printf("Usage: exe in_yuv_file_path out_rgb_file_path\n");
+	return -1;
+    }
+
+    //open files
+    sprintf(out_yuv_path,"%s-out-yuv.yuv",argv[2]);
+    yuv_file = fopen(argv[1],"r");
+    rgb_file = fopen(argv[2],"w");
+    out_yuv_file = fopen(out_yuv_path,"w");
+
+    if((!yuv_file)||(!rgb_file))
+    {
+	perror("fopen");
+	return -1;
+    }
+    if(0 > fread(yuv_buf,1,width*height*3/2,yuv_file))
+    {
+	perror("fread");
+	return -1;
+    }
+
+    //nv21 --> rgba
+    nv21_to_rgba_init();
+    gettimeofday(&start_time,NULL);
+    nv21_to_rgba(yuv_buf,rgb_buf,width,height);
+    gettimeofday(&end_time,NULL);
+    printf( "size %dx%d, nv21_to_rgba time cost : %ld us\n",width,height,measure_us(&start_time,&end_time) );
+
+    //write to rgb file
+    if(0 > fwrite(rgb_buf,1,width*height*4,rgb_file))
+    {
+	perror("fwrite");
+	return -1;
+    }
+
+    //rgba -->yuv
+    gettimeofday(&start_time,NULL);
+    rgba_to_yuv(rgb_buf,out_yuv_buf,width,height);
+    gettimeofday(&end_time,NULL);
+    printf( "size %dx%d, rgba_to_yuv time cost : %ld us\n",width,height,measure_us(&start_time,&end_time) );
+
+    //write to yuv out file
+    if(0 > fwrite(out_yuv_buf,1,width*height*3,out_yuv_file))
+    {
+	perror("fwrite");
+	return -1;
+    }
+
+    // close file handlers
+    fflush(rgb_file);
+    fflush(out_yuv_file);
+    fclose(yuv_file);
+    fclose(rgb_file);
+    fclose(out_yuv_file);
+    return 0;
+}
+
+
